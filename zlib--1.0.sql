0d3f5b3414cef8a0b854571cb9b1a591a1be4c32
Ronaldo <ronaldo.silva@lemaf.ufla.br>
2016-12-08 15:42:28 -0200

    My first test

--- zlib--1.0.sql
+++ zlib--1.0.sql
@@ -1,4 +1,9 @@
 CREATE FUNCTION zlib_decompress(bytea)
-RETURNS text
+RETURNS bytea
 AS 'MODULE_PATHNAME', 'pg_zlib_decompress'
+LANGUAGE C IMMUTABLE STRICT;
+
+CREATE FUNCTION zlib_compress(bytea, integer=-1)
+RETURNS bytea
+AS 'MODULE_PATHNAME', 'pg_zlib_compress'
 LANGUAGE C IMMUTABLE STRICT;
--- zlib.c
+++ zlib.c
@@ -16,7 +16,7 @@ Datum
 pg_zlib_decompress(PG_FUNCTION_ARGS)
 {
     bytea *data;
-    text *res;
+    bytea *res;
     int expected_rate = 6,
         zlib_res;
 
@@ -36,7 +36,7 @@ pg_zlib_decompress(PG_FUNCTION_ARGS)
         } else if (zlib_res != Z_OK) {
             ereport(ERROR,
                 (errcode(ERRCODE_EXTERNAL_ROUTINE_INVOCATION_EXCEPTION),
-                 errmsg("zlib error: %s", zError(zlib_res))));
+                 errmsg("zlib decompress error: %s", zError(zlib_res))));
         }
         break;
     } while (1);
@@ -44,5 +44,43 @@ pg_zlib_decompress(PG_FUNCTION_ARGS)
     SET_VARSIZE(res, VARHDRSZ + res_len);
 
     PG_FREE_IF_COPY(data, 0);
-    PG_RETURN_TEXT_P(res);
+    PG_RETURN_BYTEA_P(res);
+}
+
+
+Datum        pg_zlib_compress(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(pg_zlib_compress);
+
+Datum
+pg_zlib_compress(PG_FUNCTION_ARGS)
+{
+    bytea *data;
+    bytea *res;
+    int level;
+    int zlib_res;
+
+    uLongf res_len, data_len;
+
+    data = PG_GETARG_BYTEA_P(0);
+    level = PG_GETARG_INT32(1);
+
+    if (level < Z_NO_COMPRESSION)
+        level = Z_DEFAULT_COMPRESSION;
+    else if (level > Z_BEST_COMPRESSION)
+        level = Z_BEST_COMPRESSION;
+
+    data_len = VARSIZE(data) - VARHDRSZ;
+    res_len = compressBound(data_len);
+
+    res = palloc(VARHDRSZ + res_len);
+    zlib_res = compress2((Bytef*)VARDATA(res), &res_len, (Bytef*)VARDATA(data), data_len, level);
+    if (zlib_res != Z_OK) {
+        ereport(ERROR, 
+            (errcode(ERRCODE_EXTERNAL_ROUTINE_INVOCATION_EXCEPTION),
+                errmsg("zlib compress error: %s", zError(zlib_res))));
+    }
+
+    SET_VARSIZE(res, VARHDRSZ + res_len);
+    PG_FREE_IF_COPY(data, 0);
+    PG_RETURN_BYTEA_P(res);
 }
